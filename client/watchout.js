518ce0978864e8f3bcc49d2548e1cd7228e56fa0
lcbits <lynnc24@gmail.com>
2016-05-18 11:46:40 -0700

    Complete watchout project

--- client/watchout.js
+++ client/watchout.js
@@ -1,74 +1,130 @@
-// start slingin' some d3 here.
-$(document).ready(function() {
+//Create Board
+var svg = d3.select('body').append('svg');
 
-  //Create Board
+var boardSize = 1000;
 
-  //Width and height
-  var w = 1000;
-  var h = 500;
+//Create dataset for enemies' positions
+var numEnemies = 10;
+var enemyDataset = [];
 
-  var svg = d3.select("body")
-    .append("svg")
-    .attr("height", h)
-    .attr("width", w);
+for (var i = 0; i < numEnemies; i++) {
+  enemyDataset[i] = i;
+}
 
-  var dataset = [ 5, 10, 15, 20, 25 ];
+var newPositions = function() {
+  return enemyDataset.map(function(i) {
+    return {
+      enemyIndex: i,
+      x: Math.random() * boardSize,
+      y: Math.random() * boardSize
+    };
+  });
+};
+var enemyPositions = newPositions();
 
-  var circles = svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d, i){
-      return (i * 50) + 25;
+//Create enemies
+var enemies = svg.selectAll('image')
+  .data(enemyPositions)
+  .enter()
+  .append('image')
+  .attr('xlink:href', 'asteroid.png')
+  .attr('x', function(d) { return d.x })
+  .attr('y', function(d) { return d.y })
+  .attr('class', 'enemy')
+  .attr('height', 60)
+  .attr('width', 60);
+
+//Create function for enemies' random movements
+var move = function () {
+  enemies.transition()
+    .attr('x', function(d) {
+      d.x = Math.random() * boardSize;
+      return d.x;
     })
-    .attr("cy", h/2)
-    .attr("r", function(d){
-      return d;
+    .attr('y', function(d) {
+      d.y = Math.random() * boardSize;
+      return d.y;
     })
-    .attr("fill", "yellow")
-    .attr("stroke", "orange")
-    .attr("stroke-width", function(d){
-      return d/2;
-    });
+    .duration(1000);
+};
 
-  svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle");
+var moveInterval = setInterval(move, 1000);
 
-  var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13];
-  var barPadding = 20;
+//Create player
+var playerData = [{
+  x: 300,
+  y: 250
+}];
 
-  svg.selectAll("rect")
-   .data(dataset)
-   .enter()
-   .append("rect")
-   .attr("x", function(d, i) {
-     return i * (w / dataset.length);
-   }).attr("y", function(d) {
-     return h - (d * 4);
-   }).attr("width", w / dataset.length - barPadding)
-   .attr("height", function(d) {
-     return d * 4;
-   })
-    .attr("fill", "teal");
+var player = svg.append('circle')
+  .data(playerData)
+  .attr('class', 'player')
+  .attr('r', 15)
+  .attr('cx', function(d) { return d.x })
+  .attr('cy', function(d) { return d.y })
+  .attr('fill', 'yellow')
+  .attr('stroke', 'orange')
+  .attr('stroke-width', 10);
 
-});
-  // //spacebar
-  // var moveSelection = function(e) {
-  //   if (e.keyCode === 32) {
-  //     //add pixels to the character to the y axis
-  //   }
-  // };
+//Create ability to drag the player around
+var drag = d3.behavior.drag()
+  .on("drag", function(d, i) {
+    player.attr('cx', function(d) {
+      d.x = d.x + d3.event.dx
+      return d.x;
+    }) 
+      .attr('cy', function(d) {
+        d.y = d.y + d3.event.dy
+        return d.y;
+      })
+  })
+
+player.call(drag)
+
+//Settings for scoreboard
+var score = 0;
+var highScore = 0;
+var collisions = 0;
+
+//Create score tracker
+var scoreTracker = function() {
+  score++;
+  if (score > highScore) {
+    highScore = score;
+  }
+  d3.select('.current span').text(score);
+  d3.select('.high span').text(highScore);
+}
+
+var scoreUpdateInterval = setInterval(scoreTracker, 500);
+
+//Create collision detection function
+var collisionDetection = function() {
+  var checkCollision = false;
+
+  //loop through each enemy's position to calculate distance to player
+  enemies.each(function() {
+    var eachEnem = d3.select(this);
+    var p = player.data()[0];
+    var collisionDistance = Math.sqrt ( Math.pow((p.x - eachEnem.attr('x')), 2) + Math.pow((p.y - eachEnem.attr('y')), 2) );
+
+    if(collisionDistance < 15){
+      checkCollision = true;
+    }
+  });
+  if (checkCollision) {
+    score = 0;
+    collisions++;
+  }
+  d3.select('.collisions span').text(collisions);
+};
+setInterval(collisionDetection, 5);
 
 
-
-
-
-
-
-
-  // //event listener when the document is ready
-  // (function docReady() {
-  //   window.addEventListener('keydown', moveSelection);
-  // })();
+/*Learnings from the solutions lecture 
+  - set a settings object to whole all general descriptions
+  - helper functions to generate random px
+    - var pixelize = function(number){return number + 'px';}
+    - randX = function(){return pixelize(rand(settings.w-settings.r*2))};
+    - d3.range creates an array (n to -1)
+*/
